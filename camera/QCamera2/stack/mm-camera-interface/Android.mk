5a50c3488d9f25ba75f74eb6caa877e11fd2bc14
RJ Trujillo <certifiedblyndguy@gmail.com>
2020-03-03 11:40:19 -0700

    flo: camera: Use generated_kernel_headers
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>
    Change-Id: Ia359de6ef97f6e7188b013d6494c912e96ed25c0
    Signed-off-by: RJ Trujillo <certifiedblyndguy@gmail.com>

--- camera/QCamera2/stack/mm-camera-interface/Android.mk
+++ camera/QCamera2/stack/mm-camera-interface/Android.mk
@@ -24,9 +24,7 @@ LOCAL_C_INCLUDES := \
     $(LOCAL_PATH)/inc \
     $(LOCAL_PATH)/../common
 
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
-LOCAL_C_INCLUDES += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include/media
-LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
+LOCAL_HEADER_LIBRARIES := generated_kernel_headers
 
 LOCAL_C_INCLUDES += $(call project-path-for,qcom-media)/mm-core/inc
 
